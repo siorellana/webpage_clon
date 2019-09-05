85f093b406954d7cb35a85c444d20dd6d4518cc2
s.orellana.salazar <sebastianorellanasalazar@gmail.com>
2019-09-05 16:12:13 -0400

    better ignores

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -15,9 +15,9 @@ git remote add origin https://github.com/siorellana/test_clone.git
 
 echo "Current time : $now" >> timestamp.out
 
-#git add .
+git add .
 
-#git commit -am 'Initial commit bla bla'
+git commit -am "$(curl -s whatthecommit.com | grep '<p>' | cut -c 4-)"
 
 #git push -u origin master --force-with-lease
 
