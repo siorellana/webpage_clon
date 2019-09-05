89475cfa8de3c116843c9f7be01ae018c45a8fb5
s.orellana.salazar <sebastianorellanasalazar@gmail.com>
2019-09-05 12:02:55 -0400

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+git config --global user.email sebastianorellanasalazar@gmail.com
+
+git config --global user.name jenkins-ci
+
 git remote remove origin
+
 git remote add origin https://github.com/siorellana/test_clone.git
+
 git add .
+
 git commit -m "Initial commit bla bla"
+
 git push -u origin master
 
 echo "clonado"
