32d031e72681dde608fe02ffac66ff3baf78fa7a
s.orellana.salazar <sebastianorellanasalazar@gmail.com>
2019-09-05 12:15:44 -0400

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+git remote -v
+
 git remote remove origin
 
 git remote add origin https://github.com/siorellana/test_clone.git
 
+echo "Current time : $now" >> timestamp.log
+
 git add .
 
-git commit -m "Initial commit bla bla"
+git commit -am 'Initial commit bla bla'
 
 git push -u origin master
+
+cat timestamp.log
 
 echo "clonado"
