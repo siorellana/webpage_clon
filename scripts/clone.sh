01abda721041c402b7892fc64db3251c89843931
s.orellana.salazar <sebastianorellanasalazar@gmail.com>
2019-09-05 11:53:15 -0400

    Se han realizado nuevos cambios - su build id es: 20190905-11:53:15

---
+++ scripts/clone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git remote remove origin
+git remote add origin https://github.com/siorellana/test_clone.git
+git add .
+git commit -m "Initial commit bla bla"
+git push -u origin master
+
+echo "clonado"
